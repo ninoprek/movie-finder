99730572f9bccb85702a77afb6bd0c2902de0dfa
Nino Prekratic <ninoprekratic@gmail.com>
2020-10-03 18:18:45 +0200

    Initial setup is done

---
+++ src/components/Movie.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Movie',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
+<style lang="stylus">
+
+</style>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" placeholder="Enter a movie title" v-model="movieTitle">
+    <button @click="searchMovie">Submit</button>
+
+    <div>{{ movieTitle }}</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+
+  data: function () {
+    return {
+      movieTitle: '',
+      APIkey: '409e80d3'
+    }
+  },
+
+  methods: {
+    searchMovie () {
+
+      let params = { s: this.movieTitle };
+
+      this.axios
+      .get('http://www.omdbapi.com/?apikey=' + this.APIkey , { params: params })
+      .then(response => (
+        console.log('RESPONSE: ', response)
+      ))
+      .catch(error => (
+        console.log('CATCH RESPONSE: ', error)
+      ))
+    }
   }
 }
 </script>
