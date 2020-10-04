0a18f90ca9283a33f0b24930e628c70e83d21b1e
Nino Prekratic <ninoprekratic@gmail.com>
2020-10-04 23:21:09 +0200

    Movie details are displayed on a separate page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,37 @@
 <style lang="stylus">
+  .Home
+    display: flex
+    flex-direction: column
+    margin: auto
+
+    h2
+      margin: 140px auto 100px auto
+
+    &--input
+      display: flex
+      justify-content: center
+      margin: 40px auto
+
+      input
+        font-size: 1.8em
+        padding: 5px 10px
+
+      button
+        font-size: 1.2em
+
 </style>
 
 <template>
-  <div class="home">
-    <input type="text" placeholder="Enter a movie title" v-model="movieTitle">
-    <button @click="searchMovies">Search</button>
-    <img src="../assets/icons/search-icon.svg" alt="Search icon">
-
-    <div>{{ movieTitle }}</div>
+  <div class="Home">
+    <h2>Welcome to a perfect place to search movie information</h2>
+    <div class="Home--input">
+      <input type="text" placeholder="Enter a movie title" v-model="movieTitle">
+      <button @click="searchMovies" :disabled="buttonDisabled">SEARCH</button>
+    </div>
   </div>
 </template>
 
 <script>
-
-// import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
@@ -23,14 +40,34 @@ export default {
   data: function () {
     return {
       movieTitle: '',
-      APIkey: '409e80d3'
+      buttonDisabled: true
     }
+  },
+
+  watch: {
+    movieTitle: function () {
+      if (this.movieTitle != '') this.buttonDisabled = false;
+      else this.buttonDisabled = true;
+    }
+  },
+
+  created () {
+    window.addEventListener('keyup', this.keyHandler)
+  },
+
+  destroyed () {
+    window.removeEventListener('keyup', this.keyHandler)
   },
 
   methods: {
     searchMovies () {
       this.$router.push({ name: 'SearchResult', query: { movieTitle: this.movieTitle } });
+    },
+
+    keyHandler (e) {
+      if (e.which == '13' && !this.buttonDisabled) this.searchMovies();
     }
+
   }
 }
 </script>
